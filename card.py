d0aaf2b8fb67138b4bf275303b193133582c5bdf
Raymann Singh <raymann.singh@gmail.com>
2023-04-14 22:21:49 -0500

    commit

--- card.py
+++ card.py
@@ -1,7 +1,15 @@
 class Card:
-    def __init__(self, suit, rank):
+    def __init__(self, suit, rank, canvas, x, y, width, height, color):
         self._suit = suit
         self._rank = rank
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(x, y, x+width, y+height, fill=color)
+        self.canvas.tag_bind(self.id, '<ButtonPress-1>', self.start_drag)
+        self.canvas.tag_bind(self.id, '<ButtonRelease-1>', self.stop_drag)
+        self.canvas.tag_bind(self.id, '<B1-Motion>', self.on_drag)
+        self.dragging = False
+        self.start_x = 0
+        self.start_y = 0
 
     def getSuit(self):
         return self._suit
@@ -29,5 +37,18 @@ class Card:
 
     def __str__(self):
         return f"{self._rank} of {self._suit}"
+    
+    def start_drag(self, event):
+        self.dragging = True
+        self.start_x = event.x
+        self.start_y = event.y
 
+    def stop_drag(self, event):
+        self.dragging = False
+
+    def on_drag(self, event):
+        if self.dragging:
+            x = self.canvas.coords(self.id)[0] + event.x - self.start_x
+            y = self.canvas.coords(self.id)[1] + event.y - self.start_y
+            self.canvas.coords(self.id, x, y, x+self.width, y+self.height)
 
