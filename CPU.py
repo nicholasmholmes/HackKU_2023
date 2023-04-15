69f1161e62ae0d28668f2f0331750772a56a3ce4
Alex Doehring <129962613+AlexDoehring@users.noreply.github.com>
2023-04-14 22:35:51 -0500

    Update CPU.py

--- CPU.py
+++ CPU.py
@@ -5,21 +5,8 @@ import random
 class CPU:
     def __init__(self):
         self.hand = []
-        self.deck = Deck()
 
-    def receive_cards(self, num_cards):
-        for i in range(num_cards):
-            card = self.deck.deal()
-            self.hand.append(card)
-
-    def play_card(self):
-        return self.hand.pop()
-
-    def show_hand(self):
-        print("CPU hand:")
-        for card in self.hand:
-            print(card)
-
-    def choose_card(self):
-        # Choose a random card to play
-        return random.choice(self.hand)
+    def play_card(self, index):
+        value = self.hand[index]
+        self.hand.remove(index)
+        return value
