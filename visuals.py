ca4aaa0fca4e417d405e0224441bdd87f0680889
Raymann Singh <raymann.singh@gmail.com>
2023-04-14 22:18:20 -0500

    commit

--- visuals.py
+++ visuals.py
@@ -3,8 +3,11 @@ import os
 
 root = tk.Tk()
 
-root.title("My Window")
-root.geometry("300x200")
+canvas = tk.Canvas(root, width=400, height=400)
+canvas.pack()
+
+# Create a card
+card = Card(canvas, 50, 50, 100, 150, 'red')
 
 
 # Should be last
