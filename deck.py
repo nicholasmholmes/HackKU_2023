998a442728982bd76320dfa321acfdcd0a7ed142
Alex Doehring <129962613+AlexDoehring@users.noreply.github.com>
2023-04-14 22:20:27 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -7,8 +7,14 @@ class Deck:
         ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
+        self.discards = []
 
+    def add_to_deck(self, car):
+        self.cards.append(car)
+    
     def shuffle(self):
+        while len(self.discards) != 0:
+            self.add_to_deck(self.discards.pop())
         random.shuffle(self.cards)
 
     def deal(self):
