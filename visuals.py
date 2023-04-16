63b0fe870cd8af742a4e492a407cb99601e35877
Raymann Singh <raymann.singh@gmail.com>
2023-04-15 22:28:44 -0500

    commit

--- visuals.py
+++ visuals.py
@@ -11,61 +11,6 @@ game.setup()
 print(game.player_list[0].hand)
 win = False
 
-# Loading card images
-suit_list = ['spades', 'hearts', 'clubs', 'diamonds']
-numbers_list = ['2','3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
-key_numbers_list = ['two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace']
-# Load multiple images
-card_image_list = dict()
-for i in range(4):
-        for j in range(13):
-            filename = f'{numbers_list[j]}_of_{suit_list[i]}.png'  # Replace with your image file name
-            image = pygame.image.load(filename)
-            card_image_list[f'{key_numbers_list[j]} of {suit_list[i]}'] = image
-        
-draw_pile_img = pygame.image.load('back_of_card.png')
-draw_pile_img = pygame.transform.scale(draw_pile_img, (106, 146))
-window.blit(draw_pile_img, (sw/2-106, sh/2-73))
-
-discard_pile_img = image_list[str(game._deck.discard_pile[len(game._deck.discard_pile)-1]).lower()]
-discard_pile_img = pygame.transform.scale(discard_pile_img, (100, 140))
-
-window.blit(draw_pile_img, (sw/2-30, 0))
-window.blit(draw_pile_img, (sw/2-50, 0))
-window.blit(draw_pile_img, (sw/2-70, 0))
-window.blit(draw_pile_img, (sw/2-90, 0))
-
-draw_pile_img = pygame.transform.rotate(draw_pile_img, 90)
-window.blit(draw_pile_img, (0, sh/2-30))
-window.blit(draw_pile_img, (0, sh/2-50))
-window.blit(draw_pile_img, (0, sh/2-70))
-window.blit(draw_pile_img, (0, sh/2-90))
-
-window.blit(draw_pile_img, (sw-140, sh/2-90))
-window.blit(draw_pile_img, (sw-140, sh/2-70))
-window.blit(draw_pile_img, (sw-140, sh/2-50))
-window.blit(draw_pile_img, (sw-140, sh/2-30))
-
-window.blit(discard_pile_img, (sw/2, sh/2-70))
-
-pygame.display.update()
-
-# Example of how to print cards
-'''
-counter_width = 0
-counter_length = 0
-for card in game.player_list[0].hand:
-    card = pygame.transform.scale(card_image_list[str(card).lower()], (100, 140))
-    window.blit(card, (counter_width, counter_length))
-    pygame.display.update()
-    counter_width += 100
-    if counter_width == 900:
-        counter_width = 0
-        counter_length += 140
-print(game.player_list[0].hand)
-'''
-
-
 # Set window size
 screen_info = pygame.display.Info()
 sw = screen_info.current_w
